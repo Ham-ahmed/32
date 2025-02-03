ac5e30dc292277f3b3b28a1cce958255388ae55b
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2025-02-03 05:16:07 +0200

    Add files via upload

---
+++ youtube_py3-git1273.sh
@@ -0,0 +1,27 @@
+
+#!/bin/sh
+#
+
+wget -O /var/volatile/tmp/enigma2-plugin-extensions-youtube_py3-git1273-c5877dd-r0.0.ipk "https://raw.githubusercontent.com/Ham-ahmed/32/refs/heads/main/enigma2-plugin-extensions-youtube_py3-git1273-c5877dd-r0.0.ipk"
+wait
+opkg install --force-overwrite /tmp/*.ipk
+wait
+rm -f /var/volatile/tmp/enigma2-plugin-extensions-youtube_py3-git1273-c5877dd-r0.0.ipk
+wait
+sleep 2;
+echo "" 
+echo "" 
+echo "**********************************************************
+echo "#                   INSTALLED SUCCESSFULLY              #"
+echo "*                       ON - Panel                      *"
+echo "*                Enigma2 restart is required            *"
+echo "**********************************************************
+echo "               UPLOADED BY  >>>>   HAMDY_AHMED           "
+sleep 4;
+	echo '======================================================
+############################################                                                                                                                  
+echo ". >>>>         RESTARING         <<<<"
+echo "**********************************************************
+wait
+killall -9 enigma2
+exit 0
